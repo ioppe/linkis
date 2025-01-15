e9f3d072a8e4ca5fb184df49b6cc3f6c30baa527
sky <273606283@qq.com>
2025-01-15 10:26:57 +0800

    去掉Hive的安全检查

--- linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/hiveJdbc/SqlConnection.java
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/hiveJdbc/SqlConnection.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.linkis.metadata.query.service.hiveJdbc;
 
 import org.apache.linkis.common.conf.CommonVars;
@@ -17,141 +34,137 @@ import org.slf4j.LoggerFactory;
 
 public class SqlConnection extends AbstractSqlConnection {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SqlConnection.class);
+  private static final Logger LOG = LoggerFactory.getLogger(SqlConnection.class);
 
-    private static final CommonVars<String> SQL_DRIVER_CLASS =
-            CommonVars.apply("wds.linkis.server.mdm.service.hive.driver", "org.apache.hive.jdbc.HiveDriver");
+  private static final CommonVars<String> SQL_DRIVER_CLASS =
+      CommonVars.apply(
+          "wds.linkis.server.mdm.service.hive.driver", "org.apache.hive.jdbc.HiveDriver");
 
-    private static final CommonVars<String> SQL_CONNECT_URL =
-            CommonVars.apply("wds.linkis.server.mdm.service.hive.url", "jdbc:hive2://%s:%s/%s");
+  private static final CommonVars<String> SQL_CONNECT_URL =
+      CommonVars.apply("wds.linkis.server.mdm.service.hive.url", "jdbc:hive2://%s:%s/%s");
 
-    private static final CommonVars<Integer> SQL_CONNECT_TIMEOUT =
-            CommonVars.apply("wds.linkis.server.mdm.service.hive.connect.timeout", 3000);
+  private static final CommonVars<Integer> SQL_CONNECT_TIMEOUT =
+      CommonVars.apply("wds.linkis.server.mdm.service.hive.connect.timeout", 3000);
 
-    private static final CommonVars<Integer> SQL_SOCKET_TIMEOUT =
-            CommonVars.apply("wds.linkis.server.mdm.service.hive.socket.timeout", 6000);
+  private static final CommonVars<Integer> SQL_SOCKET_TIMEOUT =
+      CommonVars.apply("wds.linkis.server.mdm.service.hive.socket.timeout", 6000);
 
-    public SqlConnection(
-            String host,
-            Integer port,
-            String username,
-            String password,
-            String database,
-            Map<String, Object> extraParams)
-            throws ClassNotFoundException, SQLException {
-        super(host, port, username, password, database, extraParams);
-        // security check
-        SecurityUtils.checkJdbcConnParams(host, port, username, password, database, extraParams);
-        SecurityUtils.appendHiveForceParams(extraParams);
-        connectMessage.extraParams.put("connectTimeout", SQL_CONNECT_TIMEOUT.getValue());
-        connectMessage.extraParams.put("socketTimeout", SQL_SOCKET_TIMEOUT.getValue());
+  public SqlConnection(
+      String host,
+      Integer port,
+      String username,
+      String password,
+      String database,
+      Map<String, Object> extraParams)
+      throws ClassNotFoundException, SQLException {
+    super(host, port, username, password, database, extraParams);
+  }
+
+  public List<String> getAllDatabases() throws SQLException {
+    List<String> dataBaseName = new ArrayList<>();
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SHOW DATABASES");
+      while (rs.next()) {
+        dataBaseName.add(rs.getString(1));
+      }
+    } finally {
+      closeResource(null, stmt, rs);
     }
+    return dataBaseName;
+  }
 
-    public List<String> getAllDatabases() throws SQLException {
-        List<String> dataBaseName = new ArrayList<>();
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SHOW DATABASES");
-            while (rs.next()) {
-                dataBaseName.add(rs.getString(1));
-            }
-        } finally {
-            closeResource(null, stmt, rs);
+  public List<String> getAllTables(String database) throws SQLException {
+    List<String> tableNames = new ArrayList<>();
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SHOW TABLES IN `" + database + "`");
+      while (rs.next()) {
+        tableNames.add(rs.getString(1));
+      }
+      return tableNames;
+    } finally {
+      closeResource(null, stmt, rs);
+    }
+  }
+
+  public List<MetaColumnInfo> getColumns(String database, String table)
+      throws SQLException, ClassNotFoundException {
+    List<MetaColumnInfo> columns = new ArrayList<>();
+    String columnSql = "SELECT * FROM `" + database + "`.`" + table + "` WHERE 1 = 2";
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    ResultSetMetaData meta = null;
+    try {
+      List<String> primaryKeys = getPrimaryKeys(table);
+      ps = conn.prepareStatement(columnSql);
+      rs = ps.executeQuery();
+      meta = rs.getMetaData();
+      int columnCount = meta.getColumnCount();
+      for (int i = 1; i < columnCount + 1; i++) {
+        MetaColumnInfo info = new MetaColumnInfo();
+        info.setIndex(i);
+        info.setLength(meta.getColumnDisplaySize(i));
+        info.setNullable((meta.isNullable(i) == ResultSetMetaData.columnNullable));
+        info.setName(meta.getColumnName(i));
+        info.setType(meta.getColumnTypeName(i));
+        if (primaryKeys.contains(meta.getColumnName(i))) {
+          info.setPrimaryKey(true);
         }
-        return dataBaseName;
+        columns.add(info);
+      }
+    } finally {
+      closeResource(null, ps, rs);
     }
+    return columns;
+  }
 
-    public List<String> getAllTables(String database) throws SQLException {
-        List<String> tableNames = new ArrayList<>();
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SHOW TABLES IN `" + database + "`");
-            while (rs.next()) {
-                tableNames.add(rs.getString(1));
-            }
-            return tableNames;
-        } finally {
-            closeResource(null, stmt, rs);
-        }
+  public Connection getDBConnection(ConnectMessage connectMessage, String database)
+      throws ClassNotFoundException, SQLException {
+    Class.forName(SQL_DRIVER_CLASS.getValue());
+    String url =
+        String.format(
+            SQL_CONNECT_URL.getValue(), connectMessage.host, connectMessage.port, database);
+    // deal with empty database
+    if (StringUtils.isBlank(database)) {
+      url = url.substring(0, url.length() - 1);
     }
+    if (MapUtils.isNotEmpty(connectMessage.extraParams)) {
+      String extraParamString =
+          connectMessage.extraParams.entrySet().stream()
+              .map(e -> String.join("=", e.getKey(), String.valueOf(e.getValue())))
+              .collect(Collectors.joining("&"));
+      url += "?" + extraParamString;
+    }
+    LOG.info("jdbc connection url: {}", url);
+    return DriverManager.getConnection(url, connectMessage.username, connectMessage.password);
+  }
 
-    public List<MetaColumnInfo> getColumns(String database, String table)
-            throws SQLException, ClassNotFoundException {
-        List<MetaColumnInfo> columns = new ArrayList<>();
-        String columnSql = "SELECT * FROM `" + database + "`.`" + table + "` WHERE 1 = 2";
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        ResultSetMetaData meta = null;
-        try {
-            List<String> primaryKeys = getPrimaryKeys(table);
-            ps = conn.prepareStatement(columnSql);
-            rs = ps.executeQuery();
-            meta = rs.getMetaData();
-            int columnCount = meta.getColumnCount();
-            for (int i = 1; i < columnCount + 1; i++) {
-                MetaColumnInfo info = new MetaColumnInfo();
-                info.setIndex(i);
-                info.setLength(meta.getColumnDisplaySize(i));
-                info.setNullable((meta.isNullable(i) == ResultSetMetaData.columnNullable));
-                info.setName(meta.getColumnName(i));
-                info.setType(meta.getColumnTypeName(i));
-                if (primaryKeys.contains(meta.getColumnName(i))) {
-                    info.setPrimaryKey(true);
-                }
-                columns.add(info);
-            }
-        } finally {
-            closeResource(null, ps, rs);
-        }
-        return columns;
-    }
+  public String getSqlConnectUrl() {
+    return SQL_CONNECT_URL.getValue();
+  }
 
-    public Connection getDBConnection(ConnectMessage connectMessage, String database)
-            throws ClassNotFoundException, SQLException {
-        Class.forName(SQL_DRIVER_CLASS.getValue());
-        String url =
-                String.format(
-                        SQL_CONNECT_URL.getValue(), connectMessage.host, connectMessage.port, database);
-        // deal with empty database
-        if (StringUtils.isBlank(database)) {
-            url = url.substring(0, url.length() - 1);
-        }
-        if (MapUtils.isNotEmpty(connectMessage.extraParams)) {
-            String extraParamString =
-                    connectMessage.extraParams.entrySet().stream()
-                            .map(e -> String.join("=", e.getKey(), String.valueOf(e.getValue())))
-                            .collect(Collectors.joining("&"));
-            url += "?" + extraParamString;
-        }
-        LOG.info("jdbc connection url: {}", url);
-        return DriverManager.getConnection(url, connectMessage.username, connectMessage.password);
+  @Override
+  public String generateJdbcDdlSql(String database, String table) {
+    String columnSql = String.format("SHOW CREATE TABLE %s.%s", database, table);
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    String ddl = "";
+    try {
+      ps = conn.prepareStatement(columnSql);
+      rs = ps.executeQuery();
+      if (rs.next()) {
+        ddl = rs.getString(1);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      closeResource(null, ps, rs);
     }
-
-    public String getSqlConnectUrl() {
-        return SQL_CONNECT_URL.getValue();
-    }
-
-    @Override
-    public String generateJdbcDdlSql(String database, String table) {
-        String columnSql = String.format("SHOW CREATE TABLE %s.%s", database, table);
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        String ddl = "";
-        try {
-            ps = conn.prepareStatement(columnSql);
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                ddl = rs.getString(1);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            closeResource(null, ps, rs);
-        }
-        return ddl.replaceAll("\n", "\n\t");
-    }
+    return ddl.replaceAll("\n", "\n\t");
+  }
 }
