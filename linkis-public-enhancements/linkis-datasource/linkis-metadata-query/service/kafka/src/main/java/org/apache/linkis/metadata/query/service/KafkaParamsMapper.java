41b39afa8392580859226edaee5af9011af54afb
sky <273606283@qq.com>
2025-01-15 19:43:07 +0800

    临时方案，使用hive的uris作为broker地址导入

--- linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/kafka/src/main/java/org/apache/linkis/metadata/query/service/KafkaParamsMapper.java
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/kafka/src/main/java/org/apache/linkis/metadata/query/service/KafkaParamsMapper.java
@@ -26,6 +26,8 @@ public class KafkaParamsMapper {
   public static final CommonVars<String> PARAM_KAFKA_KEYTAB =
       CommonVars.apply("wds.linkis.server.mdm.service.kafka.keytab", "keytab");
 
+  //临时解决，使用hive uris作为brokers传入
   public static final CommonVars<String> PARAM_KAFKA_BROKERS =
-      CommonVars.apply("wds.linkis.server.mdm.service.kafka.brokers", "brokers");
+    //CommonVars.apply("wds.linkis.server.mdm.service.kafka.brokers", "brokers");
+          CommonVars.apply("wds.linkis.server.mdm.service.kafka.brokers", "brokers");
 }
